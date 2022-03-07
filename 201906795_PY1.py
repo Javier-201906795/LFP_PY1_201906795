d7f1ee7452cf16dde8d8478890bed62d603856c9
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-03-07 12:10:59 -0600

    interfas grafica mejoras

--- 201906795_PY1.py
+++ 201906795_PY1.py
@@ -1,5 +1,5 @@
 #Librerias
-from tkinter import filedialog, Tk, Label, Button, Entry, Menu, Text, Menubutton
+from tkinter import filedialog, Tk, Label, Button, Entry, Menu, Text, Menubutton, PhotoImage, font
 
 
 #Test
@@ -46,47 +46,57 @@ if __name__ == "__main__":
 ventana = Tk()
 ventana.geometry("800x580")
 ventana.title("LFP_PY1_201906795.py")
+ventana.config(background = "white")
+
+#Fuentes
+Font1 = font.Font(family='Helvetica', size=12, weight='bold')
+Font2 = font.Font(family='Helvetica', size=10, weight='bold')
 
 
-lbl = Label(ventana,text="Cargar Archivo LFP")
+
+lbl = Label(ventana,text="Cargar Archivo LFP", bg="white")
+lbl['font']= Font1
 lbl.pack()
-lbl.place(x=10,y=20,width=100, height=30)
+lbl.place(x=5,y=10,width=200, height=40)
 
 
-btnsubir = Button(ventana,text="Subir archivo", command=mensaje, padx= 20, pady = 10, fg="black", bg="#B5C7F2")
+btnsubir = Button(ventana,text="Subir archivo", command=mensaje, fg="black", bg="#B5C7F2")
+btnsubir['font']= Font2
 btnsubir.pack()
-btnsubir.place(x=10,y=50,width=100, height=30)
+btnsubir.place(x=30,y=50,width=120, height=30)
 
 
 width1 = 600
 height1 = 400
-cuadrotexto = Text(ventana, width= width1, height=height1)
+cuadrotexto = Text(ventana, width= width1, height=height1, bg="#D8E0F5")
 cuadrotexto.pack(side="left")
-cuadrotexto.place(x= 10, y=100, width = width1, height=height1)
+cuadrotexto.place(x= 30, y=100, width = width1, height=height1)
 
 
 
 
 btnanalizar = Button(ventana,text="Analizar", command=mensaje, padx= 20, pady = 10, fg="black", bg="#BBF3C5")
+btnanalizar['font']= Font2
 btnanalizar.pack()
-btnanalizar.place(x=250,y=510,width=100, height=30)
+btnanalizar.place(x=280,y=510,width=100, height=30)
 
 
 btnmenu = Menubutton(ventana, text = "Menu Reportes", bg= "#F3D997",  activebackground="#DBB77D")
-    
+btnmenu['font']= Font2    
 btnmenu.menu = Menu(btnmenu, tearoff=False)  
 btnmenu["menu"]= btnmenu.menu  
+
 btnmenu.menu.add_checkbutton(label = "Reporte de Tokens ",
-                                variable = 2)  
+                                variable = 1, command=mensaje)  
 btnmenu.menu.add_checkbutton(label = "Reporte de Errores",
-                                variable = 2)
+                                variable = 2, command=mensaje)
 btnmenu.menu.add_checkbutton(label = "Manual de Usuario",
-                                variable = 3)
+                                variable = 3, command=mensaje)
 btnmenu.menu.add_checkbutton(label = "Manual Tecnico",
-                                variable = 4)                            
+                                variable = 4, command=mensaje)                            
 
 btnmenu.pack(expand=True)
-btnmenu.place(x=650,y=140,width=100, height=30)
+btnmenu.place(x=640,y=140,width=150, height=40)
 
 ventana.mainloop()
 
