7d5472045653d3b1feaf3d447edf8ad14ccd8165
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-03-19 11:31:25 -0600

    test javascript V1.0

--- testformulario.js
+++ testformulario.js
@@ -1,11 +1,13 @@
 //Obtener div donde se colocara los elementos del formulario
 divcontenido = document.getElementById("contenidoformulario");
 
-//[ LABEL ]
+//----------------------------------------------------------------
+//[L1.0 LABEL ]
 contenidoform = '<label> VALOR </label>'
 contenidoform += '<br><br>'
 
-//[ TEXTO - INPUT ]
+//----------------------------------------------------------------
+//[T1.0 TEXTO - INPUT ]
 contenidoform += '<div class="input-group input-group-sm mb-4">'+
 '<div class="input-group-prepend col-md-2">'+
 '<span class="input-group-text">VALOR</span>'+
@@ -15,19 +17,42 @@ contenidoform += '<div class="input-group input-group-sm mb-4">'+
 '</div>'+
 '</div>'
 
-//[ GRUPO RADIO - GRUPO INPUT ]
 
-//OBTENER INPUT - RADIO SELECCIONADO
+//----------------------------------------------------------------
+//[R1.0 GRUPO RADIO - GRUPO INPUT ]
+contenidoform += '<div class="col-md-2">'+
+'<span class="input-group-text">NOMBRE</span>'+
+'</div>'+
+'<br>'+
+'<div class="form-check">'+
+'<input class="form-check-input" type="radio"  name="NOMBRE">'+
+'<label class="form-check-label">'+
+'Default radio'+
+'</label>'+
+'</div>'+
+'<div class="form-check">'+
+'<input class="form-check-input" type="radio"  name="NOMBRE">'+
+'<label class="form-check-label">'+
+'Default checked radio'+
+'</label>'+
+'</div>'
+
+//1.1 OBTENER INPUT - RADIO SELECCIONADO
 Nombre = document.getElementsByName('NOMBRE')
 
 function obtenerNombre() {
-    //Obtiene los input radio 
+    //2.1 Listado con los Valores
+    listadoNombre = ["valor1","valor2","valor3"]
+
+    //3.1 Obtiene los input radio 
     var ele = document.getElementsByName('NOMBRE');
-    //Evalua uno por uno para encontrar con el seleccionado
+    //3.1.1 Evalua uno por uno para encontrar con el seleccionado
     for(i = 0; i < ele.length; i++) {
         if(ele[i].checked)
+        //3.1.2 Guarda su Valor
+        
         document.getElementById("result").innerHTML
-                = "NOMBRE: "+ele[i].value + i;
+                = "NOMBRE: "+ele[i].value + i + listadoNombre[i];
     }
 }
 
